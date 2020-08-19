95c57d2472563e838e971895aa0ad419995feb19
fayzzz <mustafo.fayz@yandex.ru>
2020-08-20 01:31:29 +0500

    Has added few features and has written factory for generating nodes by type.

--- src/oop-paradigm/src/main.js
+++ src/oop-paradigm/src/main.js
@@ -1,65 +1,11 @@
-class RenderForm {
-    static render() {
-        const form = document.createElement('fomr');
-        form.className = 'form';
-
-        document.body.append(form);
+class TodoList {
+    constructor() {
+        this.todos = [];
     }
 }
 
-class RenderFormGroup {
-    static render(formGroups) {
-        const form = document.querySelector('.form');
-
-        form.append(
-            ...formGroups.map((name) => {
-                const label = document.createElement('label');
-                label.innerHTML = name;
-
-                const input = document.createElement('input');
-                input.type = 'text';
-                input.placeholder = name;
-
-                const div = document.createElement('div');
-                div.className = 'form-group';
-                div.append(label, input);
-
-                return div;
-            }),
-        );
-    }
-}
-
-class RenderTodoListContainer {
-    render() {
-        const div = document.createElement('div');
-        div.className = 'todos-container';
-
-        document.body.appendChild(div);
-    }
-}
-
-class RenderTodos {
-    render(todos) {
-        const todosContainer = document.querySelector('.todos-container');
-
-        todosContainer.append(
-            ...todos.map(({ date, task }) => {
-                const dateView = document.createElement('div');
-                const taskView = document.createElement('div');
-
-                dateView.innerHTML = date;
-                taskView.insertBefore = task;
-
-                const div = document.createElement('div');
-                div.className = 'todo-container';
-                div.append(dateView, taskView);
-
-                return div;
-            }),
-        );
-    }
-}
+const todoList = new TodoList();
 
 RenderForm.render();
 RenderFormGroup.render(['name']);
+RenderTodoListContainer.render();
---
+++ src/oop-paradigm/src/node-factory.js
@@ -0,0 +1,82 @@
+class InputNode {
+    constructor(options) {
+        this.options = options;
+    }
+
+    render() {
+        const { value, type, id } = this.options;
+        const input = document.createElement('input');
+
+        input.placeholder = value;
+        input.type = type;
+        input.id = id;
+
+        return input;
+    }
+}
+
+class LabelNode {
+    constructor(options) {
+        this.options = options;
+    }
+
+    render() {
+        const { value } = this.options;
+        const label = document.createElement('label');
+
+        label.innerHTML = value;
+
+        return label;
+    }
+}
+
+class DivNode {
+    constructor(options) {
+        this.options = options;
+    }
+
+    render() {
+        const { value, className } = this.options;
+        const div = document.createElement('div');
+
+        div.className = className;
+
+        if (Array.isArray(value)) {
+            div.append(...value);
+        } else {
+            div.innerHTML = value;
+        }
+
+        return div;
+    }
+}
+
+class FormNode {
+    constructor(options) {
+        this.options = options;
+    }
+
+    render() {
+        const { className } = this.options;
+        const form = document.createElement('form');
+
+        form.className = className;
+
+        return form;
+    }
+}
+
+class NodeFactory {
+    createNode = (type, options) => {
+        switch (type) {
+            case 'input':
+                return new InputNode(options);
+            case 'label':
+                return new LabelNode(options);
+            case 'div':
+                return new DivNode(options);
+            case 'form':
+                return new FormNode(options);
+        }
+    };
+}
